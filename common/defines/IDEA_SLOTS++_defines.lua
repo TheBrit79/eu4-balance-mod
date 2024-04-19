8da45c0ae55bf72517e5790e9f63775648346a4d
TheBrit7 <>
2024-04-19 17:28:51 +0100

    Persia nerf

---
+++ common/defines/IDEA_SLOTS++_defines.lua
@@ -0,0 +1,3 @@
+-- NDefines.NCountry.PS_BUY_IDEA = 200 -- cost of each ideas 
+NDefines.NCountry.FREE_IDEA_GROUP_COST  = 4 -- every x.th idea, you'll unlock a new national idea
+NDefines.NCountry.IDEA_TO_TECH = -0.010			-- percentage on tech reduction per idea.
